dbbc0fff44a00c962d06ea4bab62a30543a380ef
SSS <sksakibraj15@gmail.com>
2022-10-26 16:26:19 +0600

    first frontend

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Footer Section</h2>
+            <p><small>@Copyright</small></p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBGLIpF626alBDRGjND7u3M8koYgnb4VjA",
+    authDomain: "sss-news-3fbb1.firebaseapp.com",
+    projectId: "sss-news-3fbb1",
+    storageBucket: "sss-news-3fbb1.appspot.com",
+    messagingSenderId: "36961721592",
+    appId: "1:36961721592:web:d51b2b853e2596d5192f0d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
